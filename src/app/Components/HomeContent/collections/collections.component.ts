cc50966b68d875eb06256d03a455ae548fd64962
booda2201-tech <booda2201@gmail.com>
2026-01-27 10:23:51 +0200

    auto-commit

--- src/app/Components/HomeContent/collections/collections.component.ts
+++ src/app/Components/HomeContent/collections/collections.component.ts
@@ -7,5 +7,38 @@ import { Component } from '@angular/core';
 })
 export class CollectionsComponent {
 
-  
+  categories = [
+    {
+      img: '../../../../assets/p1.png',
+      title: 'Tea Cups',
+      description: 'Where Comfort Meets Refinement.'
+    },
+    {
+      img: '../../../../assets/p2.png',
+      title: 'Tea Bots',
+      description: 'Designed to Brew Excellence.'
+    },
+    {
+      img: '../../../../assets/p3.png',
+      title: 'Matcha Mixer',
+      description: 'Savor the Essence of Matcha.'
+    }
+  ]
+
+  // constructor(private api : apiService) { }
+
+
+  // ngOnInit(): void {
+  //   this.api.getallCategories().subscribe({
+  //     next:(res:any)=>{
+  //       console.log(this.categories);
+  //       this.categories = res;
+  //     },
+  //     error:(err:any)=>{
+  //       console.log(err);
+  //     }
+  //   })
+  // }
+
+
 }
