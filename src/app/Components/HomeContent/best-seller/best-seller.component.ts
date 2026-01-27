22a8241716fa5018b56139a6c30272380ca5f4e6
booda2201-tech <booda2201@gmail.com>
2026-01-27 10:35:15 +0200

    auto-commit

--- src/app/Components/HomeContent/best-seller/best-seller.component.ts
+++ src/app/Components/HomeContent/best-seller/best-seller.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+
+interface Product {
+  name: string;
+  image: string;
+  altText: string;
+}
+
 @Component({
   selector: 'app-best-seller',
   templateUrl: './best-seller.component.html',
   styleUrls: ['./best-seller.component.scss']
 })
 export class BestSellerComponent {
+
+
+  bestSellers: Product[] = [
+    {
+      name: 'Herbal Tea',
+      image: '../../../../assets/Herbal Tea.png',
+      altText: 'Herbal Tea'
+    },
+    {
+      name: 'Black Tea',
+      image: '../../../../assets/Black Tea.png',
+      altText: 'Black Tea'
+    },
+    {
+      name: 'Green Tea',
+      image: '../../../../assets/Green Tea.png',
+      altText: 'Green Tea'
+    }
+  ];
 
 }
