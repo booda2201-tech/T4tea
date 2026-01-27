571e27c15a7d0fb8d4ca25af0e692c0ebb2a0c50
booda2201-tech <booda2201@gmail.com>
2026-01-27 13:25:10 +0200

    auto-commit

--- src/app/Components/HomeContent/collections/collections.component.ts
+++ src/app/Components/HomeContent/collections/collections.component.ts
@@ -27,24 +27,20 @@ export class CollectionsComponent {
   ];
 
 
-  //   customOptions: OwlOptions = {
-  //   loop: true,
-  //   mouseDrag: true,
-  //   touchDrag: true,
-  //   pullDrag: false,
-  //   dots: false,
-  //   navSpeed: 700,
-  //   navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-  //   responsive: {
-  //     0: { items: 1 },
-  //     740: { items: 2 },
-  //     940: { items: 3 }
-  //   },
-  //   nav: false
-  // }
-
-
-
+  customOptions: OwlOptions = {
+      loop: true,
+      mouseDrag: true,
+      touchDrag: true,
+      pullDrag: false,
+      dots: false,
+      navSpeed: 700,
+      responsive: {
+        0: { items: 1 },
+        740: { items: 2 },
+        940: { items: 3 }
+      },
+      nav: false
+  }
 
 
 
@@ -63,19 +59,6 @@ export class CollectionsComponent {
   //   })
   // }
 
-customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: false,
-    dots: false,
-    navSpeed: 700,
-    responsive: {
-      0: { items: 1 },
-      740: { items: 2 },
-      940: { items: 3 }
-    },
-    nav: false
-  }
 
+  
 }
