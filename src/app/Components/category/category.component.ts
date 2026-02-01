2061582d111495767ec37f2900fa97fbc460814b
booda2201-tech <booda2201@gmail.com>
2026-02-01 16:52:59 +0200

    auto-commit

--- src/app/Components/category/category.component.ts
+++ src/app/Components/category/category.component.ts
@@ -6,7 +6,7 @@ export interface Product {
   price: string;
   category: string;
   image: string;
-  isFavorite?: boolean; 
+  isFavorite?: boolean;
 }
 
 @Component({
@@ -18,15 +18,24 @@ export class CategoryComponent {
   categories = ['Black Tea', 'Green Tea', 'Herbal Tea'];
 
   products: Product[] = [
-    { id: 1, name: 'Moroccan Mint', price: '350 EGP', category: 'Black Tea', image: 'assets/images/mint.png', isFavorite: false },
-    { id: 2, name: 'Pomegranate', price: '350 EGP', category: 'Black Tea', image: 'assets/images/pomegranate.png', isFavorite: false },
-    { id: 3, name: 'Lavender Grey', price: '350 EGP', category: 'Black Tea', image: 'assets/images/lavender.png', isFavorite: true },
-    { id: 4, name: 'Lemon Citrus', price: '350 EGP', category: 'Green Tea', image: 'assets/images/lemon.png', isFavorite: false },
-    { id: 5, name: 'Tranquility', price: '350 EGP', category: 'Green Tea', image: 'assets/images/tranquility.png', isFavorite: false },
-    { id: 6, name: 'Peachy Green', price: '350 EGP', category: 'Green Tea', image: 'assets/images/peach.png', isFavorite: false },
-    { id: 7, name: 'Rosehip Peach', price: '350 EGP', category: 'Herbal Tea', image: 'assets/images/rosehip.png', isFavorite: false },
-    { id: 8, name: 'Blueberry Blues', price: '350 EGP', category: 'Herbal Tea', image: 'assets/images/blueberry.png', isFavorite: false },
-    { id: 9, name: 'Coconut Island', price: '350 EGP', category: 'Herbal Tea', image: 'assets/images/coconut.png', isFavorite: false },
+    { id: 1, name: 'Moroccan Mint', price: '350 EGP', category: 'Black Tea', image: '../../../assets/1 (3) 6.png', isFavorite: false },
+    { id: 2, name: 'Pomegranate', price: '350 EGP', category: 'Black Tea', image: '../../../assets/Black Tea.png', isFavorite: false },
+    { id: 3, name: 'Lavender Grey', price: '350 EGP', category: 'Black Tea', image: '../../../assets/4 (3) 4.png', isFavorite: true },
+    { id: 1, name: 'Moroccan Mint', price: '350 EGP', category: 'Black Tea', image: '../../../assets/1 (3) 6.png', isFavorite: false },
+    { id: 2, name: 'Pomegranate', price: '350 EGP', category: 'Black Tea', image: '../../../assets/Black Tea.png', isFavorite: false },
+    { id: 3, name: 'Lavender Grey', price: '350 EGP', category: 'Black Tea', image: '../../../assets/4 (3) 4.png', isFavorite: true },
+    { id: 4, name: 'Lemon Citrus', price: '350 EGP', category: 'Green Tea', image: '../../../assets/8 (2) 2.png', isFavorite: false },
+    { id: 5, name: 'Tranquility', price: '350 EGP', category: 'Green Tea', image: '../../../assets/Green Tea.png', isFavorite: false },
+    { id: 6, name: 'Peachy Green', price: '350 EGP', category: 'Green Tea', image: '../../../assets/11 (2) 2.png', isFavorite: false },
+    { id: 4, name: 'Lemon Citrus', price: '350 EGP', category: 'Green Tea', image: '../../../assets/8 (2) 2.png', isFavorite: false },
+    { id: 5, name: 'Tranquility', price: '350 EGP', category: 'Green Tea', image: '../../../assets/Green Tea.png', isFavorite: false },
+    { id: 6, name: 'Peachy Green', price: '350 EGP', category: 'Green Tea', image: '../../../assets/11 (2) 2.png', isFavorite: false },
+    { id: 7, name: 'Rosehip Peach', price: '350 EGP', category: 'Herbal Tea', image: '../../../assets/13 (1) 2.png', isFavorite: false },
+    { id: 8, name: 'Blueberry Blues', price: '350 EGP', category: 'Herbal Tea', image: '../../../assets/15 (2) 1.png', isFavorite: false },
+    { id: 9, name: 'Coconut Island', price: '350 EGP', category: 'Herbal Tea', image: '../../../assets/19 (1) 1.png', isFavorite: false },
+    { id: 7, name: 'Rosehip Peach', price: '350 EGP', category: 'Herbal Tea', image: '../../../assets/13 (1) 2.png', isFavorite: false },
+    { id: 8, name: 'Blueberry Blues', price: '350 EGP', category: 'Herbal Tea', image: '../../../assets/15 (2) 1.png', isFavorite: false },
+    { id: 9, name: 'Coconut Island', price: '350 EGP', category: 'Herbal Tea', image: '../../../assets/19 (1) 1.png', isFavorite: false },
   ];
 
   getProductsByCategory(cat: string) {
