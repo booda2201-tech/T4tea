4cdc2fa1fbc259afd461be76d16f313da4f386d3
booda2201-tech <booda2201@gmail.com>
2026-02-03 12:33:02 +0200

    auto-commit

--- src/app/Components/cart/cart.component.ts
+++ src/app/Components/cart/cart.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class CartComponent {
 
+
+  cartItems = [
+    { name: 'Pomegranate', type: 'Black Tea', price: 350, quantity: 2, image: '../../../assets/Black Tea.png' },
+    { name: 'Moroccan Mint', type: 'Black Tea', price: 350, quantity: 1, image: '../../../assets/Green Tea.png' },
+    { name: 'Pomegranate', type: 'Black Tea', price: 350, quantity: 2, image: '../../../assets/Herbal Tea.png' },
+    { name: 'Moroccan Mint', type: 'Black Tea', price: 350, quantity: 1, image: '../../../assets/15 (2) 1.png' },
+    { name: 'Pomegranate', type: 'Black Tea', price: 350, quantity: 2, image: '../../../assets/11.png' },
+    { name: 'Moroccan Mint', type: 'Black Tea', price: 350, quantity: 1, image: '../../../assets/22 (1) 2.png' },
+  ];
+
+  increase(item: any) { item.quantity++; }
+  decrease(item: any) { if(item.quantity > 1) item.quantity--; }
 }
+
+
+
