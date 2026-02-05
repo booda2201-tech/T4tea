6a11b0461cd36f7174f4ecb60a6698951250bb9e
booda2201-tech <booda2201@gmail.com>
2026-02-05 14:01:09 +0200

    auto-commit

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -6,25 +6,35 @@ import { gsap } from 'gsap';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-  isLoginMode = false; // لو true هيعرض Login، لو false هيعرض Signup زي الصورة
+export class LoginComponent implements OnInit, AfterViewInit {
+  isLoginMode = false;
 
   constructor() { }
-
   ngOnInit(): void { }
 
   ngAfterViewInit(): void {
-    // أنيميشن دخول ناعم للمربع الزجاجي
+    this.animateCard();
+  }
+
+
+  animateCard() {
     gsap.from('.auth-card', {
       opacity: 0,
-      y: 30,
+      x: -50,
       duration: 1,
       ease: 'power3.out',
-      delay: 0.5
+      delay: 0.3
     });
   }
 
   toggleMode() {
     this.isLoginMode = !this.isLoginMode;
+
+    gsap.from('.auth-form, .title', {
+      opacity: 0,
+      y: 10,
+      duration: 0.4,
+      stagger: 0.1
+    });
   }
 }
