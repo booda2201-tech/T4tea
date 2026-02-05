fe0eaee65099d3a8e3078e1c5163c2e9b1ec54a7
booda2201-tech <booda2201@gmail.com>
2026-02-05 16:54:17 +0200

    auto-commit

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -8,6 +8,8 @@ import { gsap } from 'gsap';
 })
 export class LoginComponent implements OnInit, AfterViewInit {
   isLoginMode = false;
+  showPassword = false;
+  showRePassword = false; 
 
   constructor() { }
   ngOnInit(): void { }
@@ -20,20 +22,37 @@ export class LoginComponent implements OnInit, AfterViewInit {
   animateCard() {
     gsap.from('.auth-card', {
       opacity: 0,
-      x: -50,
+      x: -350,
       duration: 1,
       ease: 'power3.out',
       delay: 0.3
     });
   }
 
-  toggleMode() {
-    this.isLoginMode = !this.isLoginMode;
-    gsap.from('.auth-form, .title', {
-      opacity: 0,
-      y: 10,
-      duration: 0.4,
-      stagger: 0.1
-    });
+toggleMode() {
+  this.isLoginMode = !this.isLoginMode;
+
+
+  gsap.fromTo('.welcome-text',
+    { opacity: 0, y: -400 },
+    { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' }
+  );
+
+  gsap.from('.input-group', {
+    opacity: 0,
+    y: 550,
+    duration: 0.6,
+    stagger: 0.3,
+    ease: 'back.out(1.7)'
+  });
+}
+
+
+togglePasswordVisibility(type: string) {
+    if (type === 'pass') this.showPassword = !this.showPassword;
+    if (type === 'repass') this.showRePassword = !this.showRePassword;
   }
+
+
+
 }
